457f8fd4201edb7fdeaac4f9f1e89c33f8bd8e46
Elijah Thomas <thomase096@gmail.com>
2024-05-21 10:14:03 -0500

    Module 3 completed baseline

--- app_server/routes/travel.js
+++ app_server/routes/travel.js
@@ -1,8 +1,10 @@
 const express = require('express');
-
+const fs = require('fs')
 const router = express.Router();
 const controller = require('../controllers/travel');
 
+
+const trips = JSON.parse(fs.readFileSync('../data/trips.json','utf8'));
 /* GET travel page. */
 router.get('/', controller.travel);
 
